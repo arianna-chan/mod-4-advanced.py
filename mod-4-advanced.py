9dc94b25ac8fee0158c7ef07a494edd5222d8386
arianna-chan <arianna.chan@obf.ateneo.edu>
2023-12-13 00:13:54 +0800

    Update mod-4-advanced.py

--- mod-4-advanced.py
+++ mod-4-advanced.py
@@ -1,83 +1,179 @@
-def relationship_status(from_member="joaquin", to_member="chums", social_graph=None):
-    if social_graph is None:
-        social_graph = {
-            "@bongolpoc": {"first_name": "Joselito", "last_name": "Olpoc", "following": []},
-            "@joaquin": {"first_name": "Joaquin", "last_name": "Gonzales", "following": ["@chums", "@jobenilagan"]},
-            "@chums": {"first_name": "Matthew", "last_name": "Uy",
-                       "following": ["@bongolpoc", "@miketan", "@rudyang", "@joeilagan"]},
-            "@jobenilagan": {"first_name": "Joben", "last_name": "Ilagan",
-                             "following": ["@eeebeee", "@joeilagan", "@chums", "@joaquin"]},
-            "@joeilagan": {"first_name": "Joe", "last_name": "Ilagan",
-                            "following": ["@eeebeee", "@jobenilagan", "@chums"]},
-            "@eeebeee": {"first_name": "Elizabeth", "last_name": "Ilagan",
-                         "following": ["@jobenilagan", "@joeilagan"]}
-        }
+'''Module 4: Individual Programming Assignment 1
 
-    if to_member in [user for user in social_graph if from_member in social_graph[user]["following"]]:
-        if from_member in [user for user in social_graph if to_member in social_graph[user]["following"]]:
-            return "friends"
-        else:
-            return "followed by"
+Parsing Data
+
+This assignment covers your ability to manipulate data in Python.
+'''
+
+def relationship_status(from_member, to_member, social_graph):
+    '''Relationship Status.
+    15 points.
+
+    Let us pretend that you are building a new app.
+    Your app supports social media functionality, which means that users can have
+    relationships with other users.
+
+    There are two guidelines for describing relationships on this social media app:
+    1. Any user can follow any other user.
+    2. If two users follow each other, they are considered friends.
+
+    This function describes the relationship that two users have with each other.
+
+    Please see "assignment-4-sample-data.py" for sample data. The social graph
+    will adhere to the same pattern.
+
+    Parameters
+    ----------
+    from_member: str
+        the subject member
+    to_member: str
+        the object member
+    social_graph: dict
+        the relationship data    
+
+    Returns
+    -------
+    str
+        "follower" if fromMember follows toMember,
+        "followed by" if fromMember is followed by toMember,
+        "friends" if fromMember and toMember follow each other,
+        "no relationship" if neither fromMember nor toMember follow each other.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    if to_member in social_graph[from_member]['following'] and from_member in social_graph[to_member]['following']:
+        answer='friends'
+    elif to_member in social_graph[from_member]['following']:
+        answer='follower'
+    elif from_member in social_graph[to_member]['following']:
+        answer='followed by'
     else:
-        if from_member in [user for user in social_graph if to_member in social_graph[user]["following"]]:
-            return "follower"
-        else:
-            return "no relationship"
+        answer='no relationship'
+    return answer
 
 
-def tic_tac_toe(board=None):
-    if board is None:
-        board = [
-            ['X', 'X', 'O'],
-            ['O', 'X', 'O'],
-            ['X', '', 'O'],
-        ]
+def tic_tac_toe(board):
+    '''Tic Tac Toe. 
+    15 points.
 
-    grid = len(board)
-    for row in board:
-        if len(set(row)) == 1:
-            return f"{set(row).pop()} is the winner"
+    Tic Tac Toe is a common paper-and-pencil game. 
+    Players must attempt to successfully draw a straight line of their symbol across a grid.
+    The player that does this first is considered the winner.
 
-    if len(set([board[x][x] for x in range(grid)])) == 1:
-        return f"{set([board[x][x] for x in range(grid)]).pop()} is the winner"
+    This function evaluates a tic tac toe board and returns the winner.
 
-    if len(set([board[grid - 1 - y][y] for y in range(grid)])) == 1:
-        return f"{set([board[y][y] for y in range(grid)]).pop()} is the winner"
+    Please see "assignment-4-sample-data.py" for sample data. The board will adhere
+    to the same pattern. The board may by 3x3, 4x4, 5x5, or 6x6. The board will never
+    have more than one winner. The board will only ever have 2 unique symbols at the same time.
 
-    vertical_board = [z for z in zip(*board)]
-    for column in vertical_board:
-        if len(set(column)) == 1:
-            return f"{set(column).pop()} is the winner"
+    Parameters
+    ----------
+    board: list
+        the representation of the tic-tac-toe board as a square list of lists
 
-    return "NO WINNER"
+    Returns
+    -------
+    str
+        the symbol of the winner or "NO WINNER" if there is no winner
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    newboard=list(board)
+    rowsum=[]
+    columnsum=[]
+    cross=[]
+    othercross=[]
+    convertcross=[]
+    totalsum=[]
+    
+    for y in range (len(newboard)):
+        for x in range (len(newboard[y])):
+            if newboard[y][x]=='O':
+                newboard[y][x]=7
+            elif board[y][x]=='X':
+                newboard[y][x]=1
+            elif board[y][x]=='':
+                newboard[y][x]=0
 
+    for x in range (len(newboard)):
+        rowsum.append(sum(newboard[x]))   
+                
+    for x in range (len(newboard)):
+        columnsum.append(sum([item[x] for item in newboard]))
+    
+    for x in range (len(newboard)):
+        cross.append(newboard[x][x])
+        
+    for x in range (len(newboard)):
+        othercross.append([item[len(newboard)-1-x] for item in newboard])
+        
+    for x in range (len(newboard)):
+        convertcross.append(othercross[x][x])
+                
+    totalsum=rowsum+columnsum
+    totalsum.append(sum(cross))
+    totalsum.append(sum(convertcross))
+    
+    if 7*len(newboard) in totalsum:
+            answer='O'
+    elif 1*len(newboard) in totalsum:
+            answer='X'
+    else:
+            answer='NO WINNER'
+            
+    return answer
 
-def eta(first_stop="upd", second_stop="admu", route_map=None):
-    if route_map is None:
-        route_map = {
-            ("upd", "admu"): {"travel_time_mins": 10},
-            ("admu", "dlsu"): {"travel_time_mins": 35},
-            ("dlsu", "upd"): {"travel_time_mins": 55}
-        }
+def eta(first_stop, second_stop, route_map):
+    '''ETA. 
+    20 points.
 
-    travel_time = 0
-    current_stop = first_stop
+    A shuttle van service is tasked to travel along a predefined circlar route.
+    This route is divided into several legs between stops.
+    The route is one-way only, and it is fully connected to itself.
 
-    while current_stop != second_stop:
-        for leg, info in route_map.items():
-            if leg[0] == current_stop:
-                travel_time += info["travel_time_mins"]
-                current_stop = leg[1]
-                break
+    This function returns how long it will take the shuttle to arrive at a stop
+    after leaving another stop.
 
-    return f"Estimated Time of Arrival: {travel_time} mins"
+    Please see the sample data file in this same folder for sample data. The route map will
+    adhere to the same pattern. The route map may contain more legs and more stops,
+    but it will always be one-way and fully enclosed.
 
+    Parameters
+    ----------
+    first_stop: str
+        the stop that the shuttle will leave
+    second_stop: str
+        the stop that the shuttle will arrive at
+    route_map: dict
+        the data describing the routes
 
-# Test the functions with the provided sample data
-print(relationship_status("joaquin", "chums"))
-print(tic_tac_toe([
-    ['X', 'X', 'O'],
-    ['O', 'X', 'O'],
-    ['X', '', 'O'],
-]))
-print(eta("upd", "admu"))
+    Returns
+    -------
+    int
+        the time it will take the shuttle to travel from first_stop to second_stop
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    newkeys=list(route_map.keys())
+    firstkeys= [item[0]for item in newkeys]
+    secondkeys=[item[1]for item in newkeys]
+    newtime=list(route_map.values())
+    answer=[]
+    final=[]
+    
+    firstposition=firstkeys.index(first_stop)
+    secondposition=secondkeys.index(second_stop)
+        
+    for x in range(len(newtime)):
+            answer=answer+list(newtime[x].values())
+    
+    if first_stop==second_stop:
+        return sum(answer)
+    elif firstposition==secondposition:
+        return answer[firstposition]
+    elif firstposition<secondposition:
+        final=answer[firstposition:secondposition+1]
+        return sum(final)
+    elif firstposition>secondposition:
+        final=answer[firstposition:]+answer[:secondposition+1]
+        return sum(final)
